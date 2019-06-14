841a4b245ee98da51a4d9eead79608d176bddbaa
Raghu Betina <raghu@firstdraft.com>
2019-06-14 19:49:08 +0000

    Whitelist columns in Diffuser for ActiveAdmin

--- app/admin/diffusers.rb
+++ app/admin/diffusers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Diffuser do
+
+ permit_params :emotions_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
