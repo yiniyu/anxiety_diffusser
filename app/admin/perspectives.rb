241d4217b43d03f32612fdc3ec0ef911e6d5e6f2
Raghu Betina <raghu@firstdraft.com>
2019-06-14 19:48:55 +0000

    Whitelist columns in Perspective for ActiveAdmin

--- app/admin/perspectives.rb
+++ app/admin/perspectives.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Perspective do
+
+ permit_params :name, :quotes, :emotion_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
