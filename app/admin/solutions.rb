99aa5fd54624de3157a0bd8f14b89470096f78d3
Raghu Betina <raghu@firstdraft.com>
2019-06-14 19:48:59 +0000

    Whitelist columns in Solution for ActiveAdmin

--- app/admin/solutions.rb
+++ app/admin/solutions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Solution do
+
+ permit_params :description, :emotion_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
